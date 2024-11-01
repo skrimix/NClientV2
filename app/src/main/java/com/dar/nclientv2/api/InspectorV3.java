c76523ab0f62f3c7416e5705051e25ac73302856
skrimix <git@skrimix.8shield.net>
2024-11-01 20:48:13 +0500

    Find correct script element

--- app/src/main/java/com/dar/nclientv2/api/InspectorV3.java
+++ app/src/main/java/com/dar/nclientv2/api/InspectorV3.java
@@ -374,7 +374,17 @@ public class InspectorV3 extends Thread implements Parcelable {
         Elements scripts = document.getElementsByTag("script");
         if (scripts.isEmpty())
             throw new InvalidResponseException();
-        String json = trimScriptTag(scripts.get(1).html());
+        String script = null;
+        for (int i = 0; i < scripts.size(); i++) {
+            String s = scripts.get(i).html();
+            if (s.contains("parse")) {
+                script = s;
+                break;
+            }
+        }
+        if (script == null)
+            throw new InvalidResponseException();
+        String json = trimScriptTag(script);
         if (json == null)
             throw new InvalidResponseException();
         Element relContainer = document.getElementById("related-container");
